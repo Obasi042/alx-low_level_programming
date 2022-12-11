06038265d3e82da8d2121b7c39bec609489a2611
Obasi042 <obasistephen77@gmail.com>
2022-12-11 04:03:28 -0800

    edited

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("int is %2d bytes \n", sizeof(short int));
+
+	printf("long int is %2d bytes \n", sizeof(long int));
+
+	printf("float is %2d bytes \n", sizeof(float));
+
+	printf("double is %2d bytes \n", sizeof(double));
+
+	printf("long double is %2d bytes \n", sizeof(long double));
+
+	return(0)
+
+}
