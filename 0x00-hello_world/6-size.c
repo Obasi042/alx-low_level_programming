16775fbe45c957fc05bd4bd718e22a2e6459f230
Obasi042 <obasistephen77@gmail.com>
2022-12-11 16:29:57 -0800

    new files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 /**
- *   * main - Prints sized of characters
- *
- *    *    *Return: Always 0 (success)
+ * * main - prints size of characeters
+ * 
+ * * return: Always 0 (success)
  */
-
-
 int main(void)
-
 {
-	printf("int is %2d bytes \n", sizeof(short int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
-	printf("long int is %2d bytes \n", sizeof(long int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 
-	printf("float is %2d bytes \n", sizeof(float));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 
-	printf("double is %2d bytes \n", sizeof(double));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 
-	printf("long double is %2d bytes \n", sizeof(long double));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	return(0)
-
+	return (0);
 }
