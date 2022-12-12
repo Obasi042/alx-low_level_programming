25a42053b25b941861d95030d17451b8a8bab0c4
Obasi042 <obasistephen77@gmail.com>
2022-12-12 17:05:41 +0100

    no 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+/*
+ * main - print a c program with puts
+ * 
+ * return: 0
+*/
 int main(void)
 
 {
 	put("\"Programming is like building a multilingual puzzle");
 
-	return(0)
+	return(0);
 }
