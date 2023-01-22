1889e07928140ba3c04a576c572c658991c7171e
Obasi042 <obasistephen77@gmail.com>
2023-01-22 15:56:36 -0800

    1st task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name
+ * argc: arument count
+ * argv: argument vector
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
